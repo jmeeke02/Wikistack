972eaa27e74e97f2c6f4633b7a4fcdf0a55cfb30
Jordan Meeker <jordanwmeeker@gmail.com>
2016-01-25 17:18:32 -0500

    initial repo for wikistack

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var swig = require('swig');
+var routes = require('./routes/');
+var bodyParser =  require('body-parser');
+
+//Swig boilerplate code
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+swig.setDefaults({cache: false});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(3000,function(){
+	console.log('server listening');
+})
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+app.use('/', routes);
