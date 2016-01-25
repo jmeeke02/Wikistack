f74e9cd772764397bc2b065d27e70eba1f02fab7
Jordan Meeker <jordanwmeeker@gmail.com>
2016-01-25 18:30:22 -0500

    updates views models and mongoDB

---
+++ models/index.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+// Notice the `mongodb` protocol; Mongo is basically a kind of server,
+// which handles database requests and sends responses. It's async!
+var Schema = mongoose.Schema;
+var pageSchema = new Schema({
+	title: {type: String, required: true},
+	urlTitle: {type: String, required: true},
+	content: {type: String, required: true},
+	date:  {type: Date, default: Date.now},
+	status: {type: String, enum: [ 'open', 'closed']},
+	author: {type: Schema.Types.ObjectId, ref: 'User'} //?
+})
+
+var userSchema = new Schema({
+	name: {type: String, required: true},
+	email: {type: String, required: true, unique: true}
+});
+
+var Page = mongoose.model('Page', pageSchema);
+var User = mongoose.model('User', userSchema);
+
+pageSchema.virtual('fullTitle').get(function(){
+	return "/wiki/" + this.urlTitle;
+});
+
+
+mongoose.connect('mongodb://localhost/wikistack'); // <= db name will be 'wikistack'
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+module.exports = {
+  Page: Page,
+  User: User
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Page = require('../models/index.js').Page;
+var User = require('../models/index.js').User;
+
 
 router.get('/', function(req, res){
 	res.render('index');
 })
 
+router.get('/:urlTitle', function(req, res){
+	page.virtual(req.params.urlTitle).get(function(){
+	res.render(this.content);
+	});
+});
+
 module.exports = router;
+
+// var page = new PageModel({
+	
+// })
